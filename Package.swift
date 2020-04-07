d558fda206eed59a70610e17d8309bc5970bff92
Antonio Favata <afavata@hiya.com>
2020-04-07 16:20:11 +0100

    Update Package.swift to Swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -27,30 +27,30 @@ let package = Package(
   ],
   targets: [
     .target(name: "LambdaEvents", dependencies: [
-      "NIO",
-      "NIOHTTP1",
-      "NIOFoundationCompat",
-      "Base64Kit"
+      .product(name: "NIO", package: "swift-nio"),
+      .product(name: "NIOHTTP1", package: "swift-nio"),
+      .product(name: "NIOFoundationCompat", package: "swift-nio"),
+      .product(name: "Base64Kit", package: "swift-base64-kit")
     ]),
     .target(name: "LambdaRuntime", dependencies: [
       "LambdaEvents",
-      "AsyncHTTPClient",
-      "NIO",
-      "NIOHTTP1",
-      "NIOFoundationCompat",
-      "Logging"
+      .product(name: "AsyncHTTPClient", package: "async-http-client"),
+      .product(name: "NIO", package: "swift-nio"),
+      .product(name: "NIOHTTP1", package: "swift-nio"),
+      .product(name: "NIOFoundationCompat", package: "swift-nio"),
+      .product(name: "Logging", package: "swift-log"),
     ]),
     .target(name: "LambdaRuntimeTestUtils", dependencies: [
-      "NIOHTTP1",
+      .product(name: "NIOHTTP1", package: "swift-nio"),
       "LambdaRuntime"
     ]),
     .testTarget(name: "LambdaRuntimeTests", dependencies: [
       "LambdaEvents",
       "LambdaRuntime",
       "LambdaRuntimeTestUtils",
-      "NIO",
-      "NIOTestUtils",
-      "Logging",
+      .product(name: "NIO", package: "swift-nio"),
+      .product(name: "NIOTestUtils", package: "swift-nio"),
+      .product(name: "Logging", package: "swift-log"),
     ])
   ]
 )
